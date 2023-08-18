08618a82b5615fca211e8ddd8324fbcc387787ea
aozp73 <aozp73@naver.com>
2023-08-18 10:23:32 +0900

    자료구조1 - 2문제

---
+++ 200-자료구조1/덱.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dq = deque()
+
+for _ in range(n):
+    command = input().split()
+
+    if command[0] == "push_front":
+        dq.appendleft(command[1])
+    elif command[0] == "push_back":
+        dq.append(command[1])
+    elif command[0] == "pop_front":
+        print(dq.popleft() if dq else -1)
+    elif command[0] == "pop_back":
+        print(dq.pop() if dq else -1)
+    elif command[0] == "size":
+        print(len(dq))
+    elif command[0] == "empty":
+        print(1 if not dq else 0)
+    elif command[0] == "front":
+        print(dq[0] if dq else -1)
+    elif command[0] == "back":
+        print(dq[-1] if dq else -1)
