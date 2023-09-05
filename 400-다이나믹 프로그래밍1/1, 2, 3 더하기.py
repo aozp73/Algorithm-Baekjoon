0e1adfa9608111882b7cc851f4960f834fe37b20
aozp73 <aozp73@naver.com>
2023-09-05 11:12:25 +0900

    다이나믹 프로그래밍 1 - 2문제

---
+++ 400-다이나믹 프로그래밍1/1, 2, 3 더하기.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input().rstrip()) 
+
+    dp = [0] * (n + 1)
+    
+    if n >= 1:
+        dp[1] = 1
+    if n >= 2:
+        dp[2] = 2  # 1+1, 2
+    if n >= 3:
+        dp[3] = 4  # 1+1+1, 1+2, 2+1, 3
+    
+    for i in range(4, n+1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    
+    print(dp[n])
