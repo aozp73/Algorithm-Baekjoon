3ddfa856da478f03e4427bfb12466691980f765e
aozp73 <aozp73@naver.com>
2023-10-20 22:10:51 +0900

    브루트 포스 - 1문제

---
+++ 531-브루트 포스-재귀(연습)/연산자 끼워넣기 (2).py
@@ -0,0 +1,37 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
+
+min_val = 1e9 
+max_val = -1e9 
+
+def dfs(i, now):
+    global min_val, max_val, add, sub, mul, div
+    if i == N:
+        min_val = min(min_val, now)
+        max_val = max(max_val, now)
+    else:
+        if add > 0:
+            add -= 1
+            dfs(i + 1, now + numbers[i])
+            add += 1
+        if sub > 0:
+            sub -= 1
+            dfs(i + 1, now - numbers[i])
+            sub += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i + 1, now * numbers[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            if now < 0:
+                dfs(i + 1, -(-now // numbers[i]))
+            else:
+                dfs(i + 1, now // numbers[i])
+            div += 1
+
+dfs(1, numbers[0])
+
+print(max_val)
+print(min_val)
